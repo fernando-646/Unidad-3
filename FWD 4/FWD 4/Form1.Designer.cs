08a78a840d3be88a301e745c8630c75024ddc7c6
Fernando Ramos Cota <79738655+fernando-646@users.noreply.github.com>
2025-04-04 20:40:33 -0500

    Add files via upload

---
+++ FWD 4/FWD 4/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace FWD_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(434, 102);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 304);
+            listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(89, 102);
+            label1.Name = "label1";
+            label1.Size = new Size(283, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Genera los números del 1 al 20 con While";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(89, 164);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 103);
+            button1.TabIndex = 3;
+            button1.Text = "Imprimir en pantalla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.AntiqueWhite;
+            ClientSize = new Size(624, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
