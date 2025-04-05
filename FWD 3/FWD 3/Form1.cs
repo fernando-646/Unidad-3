749bef1e5c20adcd08df573f5ecd2020ae584cdb
Fernando Ramos Cota <79738655+fernando-646@users.noreply.github.com>
2025-04-04 20:40:00 -0500

    Add files via upload

---
+++ FWD 3/FWD 3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FWD_3
+{
+    public partial class Form1: Form
+    {
+        float f_num, Total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            f_num = float.Parse(textBox1.Text);
+            Total += f_num;
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Total.ToString();
+        }
+    }
+}
