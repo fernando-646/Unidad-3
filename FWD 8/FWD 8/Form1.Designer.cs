264d4816c6e97516005b3f7518a238c0f61d72d8
Fernando Ramos Cota <79738655+fernando-646@users.noreply.github.com>
2025-04-04 20:47:21 -0500

    Add files via upload

---
+++ FWD 8/FWD 8/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FWD_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(563, 281);
+            button1.Name = "button1";
+            button1.Size = new Size(126, 44);
+            button1.TabIndex = 0;
+            button1.Text = "Listar numeros";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(389, 124);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 264);
+            listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(75, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(412, 38);
+            label1.TabIndex = 2;
+            label1.Text = "Este programa escribe del 1 al 5";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MistyRose;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private Label label1;
+    }
+}
